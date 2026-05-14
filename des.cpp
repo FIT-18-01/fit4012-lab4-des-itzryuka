ed790ad1fadd9e49f1a181b9cfe9f893bdf9011e
Quang Lùn <quangspinner12@gmail.com>
2026-05-15 01:44:30 +0700

    v1.3

--- des.cpp
+++ des.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <bitset>
 #include <vector>
+
 using namespace std;
 
 // Helper function: Covert decimal to 4-bit binary string
@@ -18,7 +19,7 @@ int convert_binary_to_decimal(const string& binary) {
 string Xor(const string& a, const string& b) {
     string result = "";
     for (size_t i = 0; i < b.size(); i++) {
-        result += (a[i] != b[i]) ? '1': '0';
+        result += (a[i] != b[i]) ? '1' : '0';
     }
     return result;
 }
@@ -70,26 +71,26 @@ private:
 
     // PC1 Table
     const int pc1[56] = {
-        57,49,41,33,25,17,9, 
-        1,58,50,42,34,26,18, 
-        10,2,59,51,43,35,27, 
-        19,11,3,60,52,44,36,		 
-        63,55,47,39,31,23,15, 
-        7,62,54,46,38,30,22, 
-        14,6,61,53,45,37,29, 
-        21,13,5,28,20,12,4 
+        57,49,41,33,25,17,9,
+        1,58,50,42,34,26,18,
+        10,2,59,51,43,35,27,
+        19,11,3,60,52,44,36,
+        63,55,47,39,31,23,15,
+        7,62,54,46,38,30,22,
+        14,6,61,53,45,37,29,
+        21,13,5,28,20,12,4
     };
 
     // PC2 Table
-    const int pc2[48] = { 
-        14,17,11,24,1,5, 
-        3,28,15,6,21,10, 
-        23,19,12,4,26,8, 
-        16,7,27,20,13,2, 
-        41,52,31,37,47,55, 
-        30,40,51,45,33,48, 
-        44,49,39,56,34,53, 
-        46,42,50,36,29,32 
+    const int pc2[48] = {
+        14,17,11,24,1,5,
+        3,28,15,6,21,10,
+        23,19,12,4,26,8,
+        16,7,27,20,13,2,
+        41,52,31,37,47,55,
+        30,40,51,45,33,48,
+        44,49,39,56,34,53,
+        46,42,50,36,29,32
     };
 
     string shift_left_once(const string& key_chunk) {
@@ -122,7 +123,8 @@ public:
             if (i == 0 || i == 1 || i == 8 || i == 15) {
                 left = shift_left_once(left);
                 right = shift_left_once(right);
-            } else {
+            }
+            else {
                 left = shift_left_twice(left);
                 right = shift_left_twice(right);
             }
@@ -137,9 +139,6 @@ public:
             }
 
             roundKeys.push_back(roundKey);
-
-            // Optional: print key
-            cout << "Key " << i + 1 << ": " << roundKey << endl;
         }
     }
 
@@ -150,163 +149,171 @@ public:
 
 // Class to perform DES Feistel rounds
 class DES {
-    private:
-        // Expansion table to expand 32 bits to 48 bits
-        const int expansion_table[48] = {
-            32,1,2,3,4,5,4,5,
-            6,7,8,9,8,9,10,11,
-            12,13,12,13,14,15,16,17,
-            16,17,18,19,20,21,20,21,
-            22,23,24,25,24,25,26,27,
-            28,29,28,29,30,31,32,1
-        };
-    
-        // Permutation table after S-box substitution
-        const int permutation_tab[32] = {
-            16,7,20,21,29,12,28,17,
-            1,15,23,26,5,18,31,10,
-            2,8,24,14,32,27,3,9,
-            19,13,30,6,22,11,4,25
-        };
-    
-        // S-boxes (8 boxes)
-        const int substition_boxes[8][4][16] = {{ 
-            14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7, 
-            0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8, 
-            4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0, 
-            15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13 
-        }, 
-        { 
-            15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10, 
-            3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5, 
-            0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15, 
-            13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9 
-        }, 
-        { 
-            10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8, 
-            13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1, 
-            13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7, 
-            1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12 
-        }, 
-        { 
-            7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15, 
-            13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9, 
-            10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4, 
-            3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14 
-        }, 
-        { 
-            2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9, 
-            14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6, 
-            4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14, 
-            11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3 
-        }, 
-        { 
-            12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11, 
-            10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8, 
-            9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6, 
-            4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13 
-        }, 
-        { 
-            4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1, 
-            13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6, 
-            1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2, 
-            6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12 
-        }, 
-        { 
-            13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7, 
-            1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2, 
-            7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8, 
-            2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11 
-        }};
-    
-        vector<string> round_keys;
-    
-    public:
-        DES(const vector<string>& keys) : round_keys(keys) {}
-    
-        string encrypt(const string& input) {
-            // Apply initial permutation outside class
-            string perm = initial_permutation(input);
-    
-            // Split into left and right parts
-            string left = perm.substr(0, 32);
-            string right = perm.substr(32, 32);
-    
-            // 16 Feistel rounds
-            for (int i = 0; i < 16; i++) {
-                // Expand right half to 48 bits
-                string right_expanded = "";
-                for (int j = 0; j < 48; j++) {
-                    right_expanded += right[expansion_table[j] - 1];
-                }
-    
-                // XOR with round key
-                string xored = Xor(round_keys[i], right_expanded);
-    
-                // S-box substitution
-                string res = "";
-                for (int j = 0; j < 8; j++) {
-                    string row1 = xored.substr(j * 6, 1) + xored.substr(j * 6 + 5, 1);
-                    int row = convert_binary_to_decimal(row1);
-    
-                    string col1 = xored.substr(j * 6 + 1, 4);
-                    int col = convert_binary_to_decimal(col1);
-    
-                    int val = substition_boxes[j][row][col];
-                    res += convert_decimal_to_binary(val);
-                }
-    
-                // Permutation after S-box
-                string perm2 = "";
-                for (int j = 0; j < 32; j++) {
-                    perm2 += res[permutation_tab[j] - 1];
-                }
-    
-                // XOR permuted result with left, then swap
-                string new_right = Xor(perm2, left);
-                left = right;
-                right = new_right;
+private:
+    // Expansion table to expand 32 bits to 48 bits
+    const int expansion_table[48] = {
+        32,1,2,3,4,5,4,5,
+        6,7,8,9,8,9,10,11,
+        12,13,12,13,14,15,16,17,
+        16,17,18,19,20,21,20,21,
+        22,23,24,25,24,25,26,27,
+        28,29,28,29,30,31,32,1
+    };
+
+    // Permutation table after S-box substitution
+    const int permutation_tab[32] = {
+        16,7,20,21,29,12,28,17,
+        1,15,23,26,5,18,31,10,
+        2,8,24,14,32,27,3,9,
+        19,13,30,6,22,11,4,25
+    };
+
+    // S-boxes (8 boxes)
+    const int substition_boxes[8][4][16] = { {
+        14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,
+        0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,
+        4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,
+        15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13
+    },
+    {
+        15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10,
+        3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5,
+        0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15,
+        13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9
+    },
+    {
+        10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8,
+        13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1,
+        13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7,
+        1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12
+    },
+    {
+        7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15,
+        13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9,
+        10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4,
+        3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14
+    },
+    {
+        2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9,
+        14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6,
+        4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14,
+        11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3
+    },
+    {
+        12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,
+        10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,
+        9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,
+        4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13
+    },
+    {
+        4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,
+        13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,
+        1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,
+        6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12
+    },
+    {
+        13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,
+        1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,
+        7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,
+        2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11
+    } };
+
+    vector<string> round_keys;
+
+    string feistel_network(string input, bool is_decrypt) {
+        string perm = initial_permutation(input);
+
+        string left = perm.substr(0, 32);
+        string right = perm.substr(32, 32);
+
+        for (int i = 0; i < 16; i++) {
+            string right_expanded = "";
+            for (int j = 0; j < 48; j++) {
+                right_expanded += right[expansion_table[j] - 1];
             }
-    
-            // Swap final halves
-            string combined_text = right + left;
-    
-            // Apply inverse initial permutation outside class
-            string ciphertext = inverse_initial_permutation(combined_text);
-    
-            return ciphertext;
+
+            // Nếu là giải mã (decrypt), duyệt mảng key ngược (từ 15 về 0)
+            int key_index = is_decrypt ? (15 - i) : i;
+            string xored = Xor(round_keys[key_index], right_expanded);
+
+            string res = "";
+            for (int j = 0; j < 8; j++) {
+                string row1 = xored.substr(j * 6, 1) + xored.substr(j * 6 + 5, 1);
+                int row = convert_binary_to_decimal(row1);
+
+                string col1 = xored.substr(j * 6 + 1, 4);
+                int col = convert_binary_to_decimal(col1);
+
+                int val = substition_boxes[j][row][col];
+                res += convert_decimal_to_binary(val);
+            }
+
+            string perm2 = "";
+            for (int j = 0; j < 32; j++) {
+                perm2 += res[permutation_tab[j] - 1];
+            }
+
+            string new_right = Xor(perm2, left);
+            left = right;
+            right = new_right;
         }
+
+        string combined_text = right + left;
+        return inverse_initial_permutation(combined_text);
+    }
+
+public:
+    DES(const vector<string>& keys) : round_keys(keys) {}
+
+    string encrypt(const string& input) {
+        return feistel_network(input, false);
+    }
+
+    string decrypt(const string& input) {
+        return feistel_network(input, true);
+    }
 };
-    
+
 // Main function
 int main() {
-    // Example plaintext (64 bits)
-    string plaintext = "0001001000110100010101100111100010011010101111001101111011110001";
-    
-    // Example key (64 bits)
-    string key = "0001001100110100010101110111100110011011101111001101111111110001";
-    
-    // Generate round keys
-    KeyGenerator keygen(key);
-    keygen.generateRoundKeys(); 
-    
-    vector<string> roundKeys = keygen.getRoundKeys();
-    
-    // Create DES object
-    DES des(roundKeys);
-    
-    // Encrypt
-    string ciphertext = des.encrypt(plaintext);
-    
-    cout << "Ciphertext: " << ciphertext << endl;
-    
+    int mode;
+    // Nhận mode từ bàn phím (để CI nhận diện được dấu hiệu nhập liệu)
+    if (!(cin >> mode)) return 0;
+
+    if (mode == 1) {
+        // Mode 1: DES encrypt
+        string plaintext, key;
+        cin >> plaintext >> key;
+
+        // TODO: Đưa logic mã hóa DES hiện tại của bạn vào đây
+        // Gợi ý:
+        // KeyGenerator keygen(key);
+        // keygen.generateRoundKeys(); 
+        // DES des(keygen.getRoundKeys());
+        // string ciphertext = des.encrypt(plaintext);
+        // cout << ciphertext << endl; 
+    }
+    else if (mode == 2) {
+        // Mode 2: DES decrypt
+        string ciphertext, key;
+        cin >> ciphertext >> key;
+
+        // TODO: Gọi hàm giải mã
+    }
+    else if (mode == 3) {
+        // Mode 3: TripleDES encrypt
+        string plaintext, k1, k2, k3;
+        cin >> plaintext >> k1 >> k2 >> k3;
+
+        // TODO: Gọi mã hóa 3DES
+    }
+    else if (mode == 4) {
+        // Mode 4: TripleDES decrypt
+        string ciphertext, k1, k2, k3;
+        cin >> ciphertext >> k1 >> k2 >> k3;
+
+        // TODO: Gọi giải mã 3DES
+    }
+
     return 0;
 }
-
-    
-
-
-
-
-
-
